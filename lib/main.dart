a85f1b76c6c089ae08ba9574e11f9aac750e1cc2
thomas <grothedev@gmail.com>
2019-04-28 21:31:21 -0500

    finished first flutter tutorial. now doing some more. figure out navigation, api calls, preferences in sqlite, state, user accounts

---
+++ lib/main.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(FrogPondApp());
+
+class FrogPondApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FrogPond',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => HomeScreen(),
+        '/feed': (context) => FeedScreen(),
+      },
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: Colors.green
+      ),
+      //home: HomeScreen(), //TODO pass in title or other info to this?
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Welcome')
+      ),
+      body: Center(
+        child: RandomWords()
+      )
+    );
+  }
+}
+
+//fp: FeedScreen
+class RandomWordsState extends State<RandomWords>{
+
+  final suggestions = <WordPair>[];
+  final bigFont = const TextStyle(fontSize: 18.0);  
+  final favs = Set<WordPair>(); //saved favorite words
+
+  //fp: get config from sqlite, update if necessary
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        
+        leading: 
+          IconButton(icon: Icon(Icons.list), onPressed: pushSaved,
+           ),
+        
+        title: Text('word pair generator'),
+      ),
+      
+      body: buildSuggestions()
+    );
+    
+  }
+
+  void pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context){
+          final Iterable<ListTile> tiles = favs.map(
+            (WordPair p){
+              return ListTile(
+                title: Text(
+                  p.asPascalCase,
+                  style: bigFont
+                )
+              );
+            }
+          );
+          final List<Widget> divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          //FP: simply a custom ListView of the few screens, w/ onTap. although their might be a more standar way to deal with screen navigation
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('saved suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget buildSuggestions(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i){
+        if (i.isOdd) return Divider();
+        final index = i ~/ 2;
+        if (index >= suggestions.length){
+          suggestions.addAll(generateWordPairs().take(10));
+        }
+        return buildRow(suggestions[index]);
+      }
+    );
+  }
+
+  Widget buildRow(WordPair wp){
+    final bool saved = favs.contains(wp);
+    return ListTile(
+      title: Text(
+        wp.asPascalCase,
+        style: bigFont,
+      ), 
+      trailing: Icon(
+        saved ? Icons.favorite : Icons.favorite_border,
+        color: saved ? Colors.red : null,
+      ),
+      onTap: (){ //fp: up/down vote and make necessary API call, expand to see comments (go to comment screen)
+        setState((){ //note: here i have to manually update state by modifying an instance variable of the current RandomWordsState
+          if (saved){ 
+            favs.remove(wp);
+          } else{
+            favs.add(wp);
+          }
+        });
+      },
+    );
+
+  }
+}
+
+class RandomWords extends StatefulWidget{
+  @override
+  RandomWordsState createState() => RandomWordsState();
+}
+
+class FeedScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Tha Pond')
+      ),
+      body: Text(
+         'here would be a list with item builder making a fancy disply for each croak'
+      ),
+      //FP: floating action button for croaking
+    );
+  }
+}
